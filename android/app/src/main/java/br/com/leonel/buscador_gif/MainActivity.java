7d99d6618bdd958d9dabf4062d1025838ac853f1
Samuel Leonel <samueleonelfreitas@hotmail.com>
2019-07-01 23:01:28 -0300

    Finalização buscador de gif

---
+++ android/app/src/main/java/br/com/leonel/buscador_gif/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.leonel.buscador_gif;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
